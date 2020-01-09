3cef95338f8cac58a982f6fc80274fd3bcc4fcc7
gregorthebigmac <gregorthebigmac@gmail.com>
2020-01-09 14:13:25 -0600

    undo the changes from the last one. It's FUCKING WORKING!!!!

--- shutdown_script.sh
+++ shutdown_script.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-echo -n "Writing ACPI-SHUTDOWN to file /root/ubuntu_reboot_hack/last_shutdown.stat... "
-if echo "ACPI-SHUTDOWN" > /root/ubuntu_reboot_hack/last_shutdown.stat; then
-	echo "[OK]"
-else
-	echo "[FAIL]"
-	exit 99
-fi
+echo "ACPI-SHUTDOWN" > /root/ubuntu_reboot_hack/last_shutdown.stat
