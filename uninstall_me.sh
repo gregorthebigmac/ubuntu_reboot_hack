ad469a0dfbdb9a5a44bdd090724bb2c6f7d4d889
gregorthebigmac <gregorthebigmac@gmail.com>
2020-01-10 12:25:33 -0600

    found another broken symlink.

--- uninstall_me.sh
+++ uninstall_me.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Look for all files created by this program and remove them
-
 if [ -d /etc/reboot_hack ]; then
 	rm -rf /etc/reboot_hack
 fi
@@ -10,15 +9,12 @@ if [ -f /etc/rc5.d/S99reboot_hack ]; then
 	rm -rf /etc/rc5.d/S99reboot_hack
 fi
 
-if [ -f /etc/init.d/reboot_hack ]; then
-	rm -rf /etc/init.d/reboot_hack
-fi
-
-# Both of these run without checks, because the -f fails if the link is broken.
-rm -rf /bin/boot_check
-rm -rf /bin/log_shutdown_state
-
 # Restore powerbtn.sh from backup
 if [ -f /etc/acpi/powerbtn.sh.bak ]; then
 	mv /etc/acpi/powerbtn.sh.bak /etc/acpi/powerbtn.sh
 fi
+
+# All of these run without checks, because the -f fails if the link is broken.
+rm -rf /etc/init.d/reboot_hack
+rm -rf /bin/boot_check
+rm -rf /bin/log_shutdown_state
