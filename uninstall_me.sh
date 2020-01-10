8e220bcc87bf5e4535a389e1ffb0c0afd80c040c
gregorthebigmac <gregorthebigmac@gmail.com>
2020-01-10 12:33:49 -0600

    symlinks are still not being removed with uninstall script. Running debug...

--- uninstall_me.sh
+++ uninstall_me.sh
@@ -5,16 +5,32 @@ if [ -d /etc/reboot_hack ]; then
 	rm -rf /etc/reboot_hack
 fi
 
-if [ -f /etc/rc5.d/S99reboot_hack ]; then
-	rm -rf /etc/rc5.d/S99reboot_hack
-fi
-
 # Restore powerbtn.sh from backup
 if [ -f /etc/acpi/powerbtn.sh.bak ]; then
 	mv /etc/acpi/powerbtn.sh.bak /etc/acpi/powerbtn.sh
 fi
 
 # All of these run without checks, because the -f fails if the link is broken.
-rm -rf /etc/init.d/reboot_hack
-rm -rf /bin/boot_check
-rm -rf /bin/log_shutdown_state
+echo -n "Removing /bin/boot_check... "
+if rm -rf /bin/boot_check; then
+	echo "[OK]"
+else echo "[FAIL]"
+fi
+
+echo -n "Removing /bin/log_shutdown_state... "
+if rm -rf /bin/log_shutdown_state; then
+	echo "[OK]"
+else echo "[FAIL]"
+fi
+
+echo -n "Removing /etc/init.d/reboot_hack... "
+if rm -rf /etc/init.d/reboot_hack; then
+	echo "[OK]"
+else echo "[FAIL]"
+fi
+
+echo -n "Removing /etc/rc5.d/S99reboot_hack... "
+if rm -rf /etc/rc5.d/S99reboot_hack; then
+	echo "[OK]"
+else echo "[FAIL]"
+fi
