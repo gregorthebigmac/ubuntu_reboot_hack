597e22e4d572fee9cfb07b47ec3b9331fe602488
gregorthebigmac <gregorthebigmac@gmail.com>
2020-01-10 12:07:28 -0600

    TIL symlinks do not register as "files" when the link is broken.

--- uninstall_me.sh
+++ uninstall_me.sh
@@ -14,13 +14,9 @@ if [ -f /etc/init.d/reboot_hack ]; then
 	rm -rf /etc/init.d/reboot_hack
 fi
 
-if [ -f /bin/boot_check ]; then
-	rm -rf /bin/boot_check
-fi
-
-if [ -f /bin/log_shutdown_state ]; then
-	rm -rf /bin/log_shutdown_state
-fi
+# Both of these run without checks, because the -f fails if the link is broken.
+rm -rf /bin/boot_check
+rm -rf /bin/log_shutdown_state
 
 # Restore powerbtn.sh from backup
 if [ -f /etc/acpi/powerbtn.sh.bak ]; then
