fc9242d6d7626be72f1601242b9587358af3f4ce
gregorthebigmac <gregorthebigmac@gmail.com>
2020-01-10 12:48:00 -0600

    cleaned up text formatting

--- uninstall_me.sh
+++ uninstall_me.sh
@@ -4,8 +4,8 @@
 if [ -d /etc/reboot_hack ]; then
 	echo -n "Removing dir /etc/reboot_hack... "
 	if rm -rf /etc/reboot_hack; then
-		echo "[OK]"
-	else echo "[FAIL]"
+		echo "               [OK]"
+	else echo "              [FAIL]"
 	fi
 fi
 
@@ -13,32 +13,32 @@ fi
 if [ -f /etc/acpi/powerbtn.sh.bak ]; then
 	echo -n "Restoring /etc/acpi/powerbtn.sh from backup... "
 	if mv /etc/acpi/powerbtn.sh.bak /etc/acpi/powerbtn.sh; then
-		echo "[OK]"
-	else echo "[FAIL]"
+		echo " [OK]"
+	else echo " [FAIL]"
 	fi
 fi
 
 # All of these run without checks, because the -f fails if the link is broken.
 echo -n "Removing /bin/boot_check... "
 if rm -rf /bin/boot_check; then
-	echo "[OK]"
-else echo "[FAIL]"
+	echo "                    [OK]"
+else echo "                   [FAIL]"
 fi
 
 echo -n "Removing /bin/log_shutdown_state... "
 if rm -rf /bin/log_shutdown_state; then
-	echo "[OK]"
-else echo "[FAIL]"
+	echo "            [OK]"
+else echo "           [FAIL]"
 fi
 
 echo -n "Removing /etc/init.d/reboot_hack... "
 if rm -rf /etc/init.d/reboot_hack; then
-	echo "[OK]"
-else echo "[FAIL]"
+	echo "            [OK]"
+else echo "           [FAIL]"
 fi
 
 echo -n "Removing /etc/rc5.d/S99reboot_hack... "
 if rm -rf /etc/rc5.d/S99reboot_hack; then
-	echo "[OK]"
-else echo "[FAIL]"
+	echo "          [OK]"
+else echo "         [FAIL]"
 fi
